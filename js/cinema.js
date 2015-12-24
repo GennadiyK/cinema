5db2241dcf4ec53c3f59c71b10b2e1463e5eb969
Henadzi Kalinouski <7742589@gmail.com>
2015-12-24 17:10:04 +0300

    create object

---
+++ js/cinema.js
@@ -0,0 +1,36 @@
+var cinema = {
+	structure: {
+		employes: {
+		service: [
+			'cleanUp', 
+			'saleChicket', 
+			'operator'
+			]
+		},
+		visitors: [
+			'child', 
+			'adult'
+			],
+		tickets: {
+			price: []
+		}
+	},
+	methods: {
+		countBenefit: function() {
+
+		},
+		countSalary: function() {
+
+		},
+		showEmployes: function() {
+
+		},
+		addNewVisitor: function() {
+			
+		}
+	}
+};
+
+
+
+console.log(cinema);

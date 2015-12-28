a242a8ce337803a23e756b0af645aed6ac43f228
Henadzi Kalinouski <7742589@gmail.com>
2015-12-28 14:38:00 +0300

    fixing code

--- js/cinema.js
+++ js/cinema.js
@@ -1,47 +1,36 @@
-var cinema = {
-	structure: {
-		employes: {
-		service: [
-			'cleanUp', 
-			'saleChicket', 
-			'operator'
-			]
-		},
-		visitors: {
-			clients:[
-			'child', 
-			'adult'
-			],
-			employes:[
-			'accountant',
-			'manager',
-			'operator',
-			'cashier',
-			'security'
-			]
-			},
-		tickets: {
-			price: [
-					
-			]
-		}
-	},
-	methods: {
-		countBenefit: function() {
+var cinema = cinema || {};
 
-		},
-		countSalary: function() {
+cinema.People = function(id, name, photo) {
+	this.id = id;
+	this.name = name;
+	this.photo = photo;
+	this.visit = 0;
+	this.personal = 0;
+	this.date = '00-00-0000';
+};
 
-		},
-		showEmployes: function() {
+cinema.peopleCollection = [];
 
-		},
-		addNewVisitor: function() {
-			
-		}
+
+cinema.addPeople = function(id, name, photo) {
+	var people = new cinema.People(id.length, name, photo);
+	this.peopleCollection.push(people);
+};
+
+console.log(cinema.peopleCollection);
+cinema.addPeople(cinema.peopleCollection, "Vasya", "photo/vasya.png");
+console.log(cinema.peopleCollection);
+cinema.addPeople(cinema.peopleCollection, "Petya", "photo/petya.png");
+console.log(cinema.peopleCollection);
+
+cinema.cameraListener = function(id) {
+	var people = this.peopleCollection[id];
+	if(people) {
+		people.visit++;
+		console.log(people);
+	} else {
+		//this.addPeople(this.peopleCollection.length,'newName','photo/newPhoto.png').call(this);
+		console.log(this.addPeople(this.peopleCollection,'newName','photo/newPhoto.png').call(cinema));
 	}
 };
 
-
-
-console.log(cinema);

d62344fdf05d874d6ce2fa4ec318b4a81d0bd3e7
Henadzi Kalinouski <7742589@gmail.com>
2015-12-28 18:25:37 +0300

    created dynamic table

--- js/cinema.js
+++ js/cinema.js
@@ -6,31 +6,63 @@ cinema.People = function(id, name, photo) {
 	this.photo = photo;
 	this.visit = 0;
 	this.personal = 0;
-	this.date = '00-00-0000';
+	this.date = "00-00-0000";
 };
 
-cinema.peopleCollection = [];
+
+
+cinema.peopleCollection = JSON.parse(peoplesData);
 
 
 cinema.addPeople = function(id, name, photo) {
 	var people = new cinema.People(id.length, name, photo);
-	this.peopleCollection.push(people);
+	cinema.peopleCollection.push(people);
+	console.log(cinema.peopleCollection);
 };
 
-console.log(cinema.peopleCollection);
-cinema.addPeople(cinema.peopleCollection, "Vasya", "photo/vasya.png");
-console.log(cinema.peopleCollection);
-cinema.addPeople(cinema.peopleCollection, "Petya", "photo/petya.png");
-console.log(cinema.peopleCollection);
-
 cinema.cameraListener = function(id) {
-	var people = this.peopleCollection[id];
+	var people = cinema.peopleCollection[id];
 	if(people) {
 		people.visit++;
-		console.log(people);
 	} else {
-		//this.addPeople(this.peopleCollection.length,'newName','photo/newPhoto.png').call(this);
-		console.log(this.addPeople(this.peopleCollection,'newName','photo/newPhoto.png').call(cinema));
+		this.addPeople(cinema.peopleCollection,'newName','photo/newPhoto.png');
 	}
 };
 
+cinema.renderPeople = function() {
+	var table = document.createElement('table');
+		table.className = 'table table-bordered table-striped';
+	var thead = document.createElement('thead');
+	var tbody = document.createElement('tbody');
+	var tr, th, td;
+
+	//render header ot table
+	if(cinema.peopleCollection[0]) {
+		for(var i in cinema.peopleCollection[0]) {
+			th = document.createElement('th');
+			th.innerHTML = '<th>' + i + '</th>';
+			thead.appendChild(th);
+		}
+	}
+
+	//render body
+	for(var x = 0; x < cinema.peopleCollection.length ; x++) {
+			tr = document.createElement('tr');
+			console.log(tr);
+			for ( var key in cinema.peopleCollection[x]) {
+				td = document.createElement('td');
+				td.innerHTML = '<td>' +  cinema.peopleCollection[x][key] + '</td>';
+				tr.appendChild(td);	
+			}
+		tbody.appendChild(tr);
+	}
+				
+				
+	//render table
+	table.appendChild(thead);
+	table.appendChild(tbody);
+	document.getElementById('peoples').appendChild(table);
+
+};
+
+cinema.renderPeople();

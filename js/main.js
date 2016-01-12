2220353dd1be089730f17dfed386746cb3a10320
Henadzi Kalinouski <7742589@gmail.com>
2016-01-12 14:58:12 +0300

    creating view for model

--- js/main.js
+++ js/main.js
@@ -13,15 +13,50 @@ cinema.VisitorView = function(model, templateId) {
     var model = model;
     var template = document.getElementById(templateId);
 
+
+
+    this.editData = function(field, fieldLabel) {
+        var inputField = document.createElement('input');
+            inputField.setAttribute('class','form-control');
+        var fieldValue;
+            if(fieldLabel == 'name') {
+                fieldValue = model.name;
+                inputField.value = fieldValue;
+            }
+
+        field.innerHTML = '';
+        field.appendChild(inputField);
+        inputField.focus();
+
+        inputField.addEventListener('change', function(){
+            model.name = inputField.value;
+        });
+
+        inputField.addEventListener('blur', function(){
+            field.innerHTML = inputField.value;
+        });
+    };
+
+    this.eventListener = function(parent) {
+        parent.addEventListener('dblclick', function(e){
+            var target = e.target;
+            if(target.dataset.field != 'edit') return ;
+
+            this.editData(target,'name');
+        }.bind(cinema.visitorView));
+    };
+
     this.render = function() {
         var tr = document.createElement('tr');
         var trContent = template.innerHTML;
+        var table = document.getElementById('peoplesTable');
         for(var key in model) {
             trContent = trContent.replace(new RegExp('\{' + key + '\}'), model[key]);
         }
 
-        document.getElementById('peoplesTable').tBodies[0].innerHTML = trContent;
-    }
+        table.tBodies[0].innerHTML = trContent;
+        this.eventListener(table);
+    };
 };
 
 cinema.visitor = new cinema.Visitor('Vasya',0,'photo');

b463997fbbe4264ded1f1f3138ca92781fbaf014
Henadzi Kalinouski <7742589@gmail.com>
2016-01-13 15:07:40 +0300

    continue working with view

--- js/main.js
+++ js/main.js
@@ -23,53 +23,69 @@ cinema.VisitorView = function(model, templateId) {
         var cancelButton = document.createElement('button');
             cancelButton.setAttribute('class', 'btn btn-danger');
             cancelButton.innerHTML = 'cancel';
+        var saveButton = document.createElement('button');
+            saveButton.setAttribute('class', 'btn btn-success');
+            saveButton.innerHTML = 'save';
         var btnContainer = document.createElement('div');
             btnContainer.setAttribute('class','input-group-btn');
             btnContainer.appendChild(cancelButton);
+            btnContainer.appendChild(saveButton);
 
-        var fieldValue, newValue;
+        var fieldValue;
 
 
-
+        //checking label of col
         if(fieldLabel == 'name') {
             fieldValue = model.name;
         }
 
+        //adding text to value
         inputField.value = fieldValue;
 
+        //clearing html of col
         field.innerHTML = '';
+
+        //adding new elements: input and cancel button to the col
         inputContainer.appendChild(inputField);
         inputContainer.appendChild(btnContainer);
-
         field.appendChild(inputContainer);
 
         inputField.focus();
 
+        //canceling any changing with input value
+        cancelButton.addEventListener('click', function(e){
 
+            e.preventDefault();
+            this.cancelEditData(field, fieldValue);
 
-        cancelButton.addEventListener('click', function(){
-            inputField.value = fieldValue;
-        });
+        }.bind(this));
 
-        inputField.addEventListener('change', function(){
-            newValue = inputField.value;
-        });
+        //saving value at the moment of changing
 
-        inputField.addEventListener('blur', function(){
-            field.innerHTML = fieldValue;
+        saveButton.addEventListener('click', function(e){
 
-            if(newValue) {
-                field.innerHTML = newValue;
+            e.preventDefault();
+            if(fieldLabel == 'name') {
+                this.saveEditData(inputField, 'name', field);
             }
 
-        });
-
+        }.bind(this));
     };
+
+    this.cancelEditData = function(field, val){
+        return field.innerHTML = val;
+    };
+
+    this.saveEditData = function(input, key, field) {
+        model[key] = input.value;
+        return field.innerHTML = model[key];
+    };
+
 
     this.eventListener = function(parent) {
         parent.addEventListener('dblclick', function(e){
             var target = e.target;
-            if(target.dataset.field != 'edit') return ;
+            if(target.dataset.field != 'edit') return;
 
             this.editData(target,'name');
         }.bind(cinema.visitorView));
@@ -80,7 +96,7 @@ cinema.VisitorView = function(model, templateId) {
         var trContent = template.innerHTML;
         var table = document.getElementById('peoplesTable');
         for(var key in model) {
-            trContent = trContent.replace(new RegExp('\{' + key + '\}'), model[key]);
+            trContent = trContent.replace(new RegExp('\{\{' + key + '\}\}'), model[key]);
         }
 
         table.tBodies[0].innerHTML = trContent;

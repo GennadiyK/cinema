5a7f11e4e2579fb162e272825b97af579a3a8dd7
Henadzi Kalinouski <7742589@gmail.com>
2016-02-01 13:10:04 +0300

    created session class

--- js/session.js
+++ js/session.js
@@ -1,3 +1,31 @@
-/**
- * Created by hkalinouski on 2/1/2016.
- */
+var Session = function(id, time, idFilm, place) {
+    var placeArr = [];
+
+    this.id = id;
+    this.time = time;
+    this.idFilm = idFilm;
+    this.place = place;
+
+    this.setPlaceArr = function(x, y) {
+      for(var i = 0; i < x; i++) {
+
+          placeArr[i] = [];
+
+          for(var n = 0; n < y; n++) {
+              placeArr[i][n] = 0;
+
+          }
+      }
+        return placeArr;
+    };
+
+    this.getSessionPlace = function(numRow, numPlace){
+        return placeArr[numRow][numPlace];
+    };
+};
+
+var session = new Session(1, '13:00', 10, 2);
+
+session.setPlaceArr(4, 4);
+
+
